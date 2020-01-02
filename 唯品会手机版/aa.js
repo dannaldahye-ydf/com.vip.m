b32c569a108b72164bc9f5e29ed6c86d1b82c2ac
dannaldahye-ydf <3089253736@qq.com>
2020-01-02 17:24:26 +0800

    新增代码

---
+++ 唯品会手机版/aa.js
@@ -0,0 +1,15 @@
+var zhangsan={
+    name:"zahngsan",
+    age:19,
+    sex:"male",
+    xingwei:function () {
+        return "吃饭"
+    }
+}
+console.log(zhangsan.name);
+delete zhangsan.sex
+
+
+
+
+
